57c51f105c69fc6c085f0421cb579b6a0b4fe9a0
Alistair Duff <alistair.g.duff@gmail.com>
2018-12-15 18:12:33 +0000

    added user profiles form

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,7 +2,7 @@ class ProfilesController < ApplicationController
   
   #Get to users/user_id/profile/new
   def new
-    # Render blank profile details form
+    @profile = Profile.new
   end
   
 end
