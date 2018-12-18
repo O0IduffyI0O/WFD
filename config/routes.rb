65f638832c82bd4ba4974352f87ad1be0df9eae0
Alistair Duff <alistair.g.duff@gmail.com>
2018-12-18 15:45:06 +0000

    spacing

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  
   devise_for :users
   resources :users do
     resource :profile
   end
+  
   root to: 'pages#home'
   get 'about', to: 'pages#about'
   get 'antarctica', to: 'pages#antarctica'
