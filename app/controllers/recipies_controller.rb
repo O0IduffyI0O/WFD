f07d5472db9dc7afe8c18bed3dd740ee57f41685
Alistair Duff <alistair.g.duff@gmail.com>
2018-12-27 09:23:32 +0000

    added bootstrap thumbnail to recipies/index

--- app/controllers/recipies_controller.rb
+++ app/controllers/recipies_controller.rb
@@ -3,6 +3,17 @@ class RecipiesController < ApplicationController
   def new
     @recipy = Recipy.new
   end
+  
+  def create
+    @recipy = Recipy.new(recipy_params)
+    if @recipy.save
+      flash[:success] = "New Recipe Created"
+      redirect_to recipies_path
+    else
+      flash[:danger] = "Recipe not saved! #{@recipy.errors.full_messages.join(", ")}"
+      render action: :new
+    end
+  end
     
   def show
     @recipy = Recipy.find_by( id: params[:id] )
@@ -10,5 +21,11 @@ class RecipiesController < ApplicationController
   
   def index
   end
+  
+  private
+    def recipy_params
+      params.require(:recipy).permit(:name, :description, :avatar)
+    end
+  
     
 end
